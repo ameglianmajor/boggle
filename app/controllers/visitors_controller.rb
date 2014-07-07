7ddea3a064260ff90c81c80c82ba3a78ac9c0b57
Ameglian Major <ameglianmajor@gmail.com>
2014-07-07 03:49:27 -0700

    Added a form and view to input the Boggle board and to display the possible words.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,18 @@
 class VisitorsController < ApplicationController
+  def index
+    @length = 4
+  end
+
+  def determine_words
+    @length = 4
+    @matrix = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    @length.times do |i|
+      @length.times do |j|
+        @matrix[i][j] = params["(#{i},#{j})"]
+      end
+    end
+    bs = BoggleSolver.new(@matrix)
+    @valid_words = bs.find_all_valid_words
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
+  post 'visitors/determine_words'
 end
