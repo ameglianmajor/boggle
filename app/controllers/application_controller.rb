ab2907b886b44197382d61c27c5d63dc228bd2de
Ameglian Major <ameglianmajor@gmail.com>
2015-03-01 00:23:43 -0800

    Added error handling. Invalid input returns a json response with a 400 bad_requst status.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from "Apipie::ParamMissing", with: :handle_error
+  rescue_from "Apipie::ParamInvalid", with: :handle_error
+
+    private
+
+    def handle_error(exception)
+      respond_to do |format|
+        handle_response(exception, format)
+      end
+    end
+
+    def handle_response(e, format)
+      format.json { render json:
+        { errors: e.to_s }, status: :bad_request
+      }
+      format.html { render json:
+        { errors: e.to_s }, status: :bad_request
+      }
+    end
 end
