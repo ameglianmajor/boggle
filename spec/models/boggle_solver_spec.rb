41b94a3909fa1ed4884462b19257be0e8dd0bf5a
Ameglian Major <ameglianmajor@gmail.com>
2015-03-01 01:25:19 -0800

    DRYed out the spec.

--- spec/models/boggle_solver_spec.rb
+++ spec/models/boggle_solver_spec.rb
@@ -1,30 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe BoggleSolver, :type => :model do
+
   describe "has" do
     it "a valid factory." do
       assert(FactoryGirl.build(:boggle_solver).is_a? BoggleSolver)
     end
   end
+
   describe "method find_all_valid_words" do
     it "has 156 words." do
       test_boggle_solver = FactoryGirl.build(:boggle_solver)
       expect(test_boggle_solver.find_all_valid_words.length).to eq(156)
     end
   end
+
   describe "method valid_word?" do
     it "only finds valid words." do
       test_boggle_solver = FactoryGirl.build(:boggle_solver)
-      assert(test_boggle_solver.valid_word? 'super')
-      assert(test_boggle_solver.valid_word? 'turn')
-      assert(test_boggle_solver.valid_word? 'leo')
-      assert(test_boggle_solver.valid_word? 'pure')
-      assert(test_boggle_solver.valid_word? 'kelp')
-      assert(test_boggle_solver.valid_word? 'nuts')
-      assert(!(test_boggle_solver.valid_word? 'torn'))
-      assert(!(test_boggle_solver.valid_word? 'pest'))
+      valid_words = ['super', 'turn', 'leo', 'pure', 'kelp', 'nuts']
+      # while the word 'torn' and 'pest' are valid words, they are
+      # not in the dictionary.
+      invalid_words = ['torn', 'pest', 'qtzel', 'zelqzu']
+      valid_words.each do |word|
+        assert(test_boggle_solver.valid_word? word)
+      end
+      invalid_words.each do |word|
+        assert(!(test_boggle_solver.valid_word? word))
+      end
     end
   end
+
   describe "method start_coordinates" do
     it "finds all starting coordinates." do
       test_boggle_solver = FactoryGirl.build(:boggle_solver)
